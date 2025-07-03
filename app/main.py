a7ef93e4c21384a58eb264383b31536d689b076d
K-scorpi <mkirill602@gmail.com>
2025-07-03 15:08:20 +0500

    pravka

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 import uvicorn
 
 app = FastAPI(title="Monitoring Demo App")
+
+# Подключаем метрики
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/")
 def read_root():
@@ -10,6 +14,10 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
     return {"item_id": item_id}
+
+@app.get("/healthz")
+def health_check():
+    return {"status": "ok"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
